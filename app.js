875b19c178ed225f8aad519cc9e7d575cdea6f58
Ayyasudeen <ayyasudeen@gmail.com>
2020-09-05 19:35:34 +0530

    Done

---
+++ app.js
@@ -0,0 +1,42 @@
+const correctAnswers = ["A", "B", "A", "C"];
+const form = document.querySelector(".quiz-form");
+const result = document.querySelector(".result");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let score = 0;
+
+  const userAnswers = [
+    form.q1.value,
+    form.q2.value,
+    form.q3.value,
+    form.q4.value,
+  ];
+
+  //check answers
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]) {
+      score += 25;
+    }
+  });
+  // Show result on Page
+
+  result.classList.remove("d-none");
+  scrollTo(0, 0);
+
+  // Score animation
+  let output = 0;
+  const timer = setInterval(() => {
+    result.querySelector("span").textContent = `${output}%`;
+    if (output === score) {
+      clearInterval(timer);
+    } else {
+      output++;
+    }
+  }, 10);
+});
+
+// setTimeout(() => {
+//   alert("Hello");
+// }, 3000);
